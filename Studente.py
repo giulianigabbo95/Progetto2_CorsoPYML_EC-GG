d65aef858d2748e93ca82c736a6b145ddcc018d3
eli-carella <eli.carella@gmail.com>
2026-02-23 11:01:49 +0100

    add files

---
+++ Studente.py
@@ -0,0 +1,14 @@
+# =========================
+# CLASSE STUDENTE
+# =========================
+
+class Studente:
+    def __init__(self, nome, corso):
+        self.nome = nome
+        self.corso = corso
+
+    def to_list(self):
+        return [self.nome, self.corso]
+
+    def __str__(self):
+        return f"Nome: {self.nome} - Corso: {self.corso}"
